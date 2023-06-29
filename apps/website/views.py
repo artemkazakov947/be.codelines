0cb05bf31a9a6c4ce4c93a4ef9cf7bf071e44006
Artem Kazakov <artemkazakov947@gmail.com>
2023-06-29 14:52:47 +0200

    Created OverOnsView

--- apps/website/views.py
+++ apps/website/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from django.views import generic
+
+from apps.website.models import Employee
 
 
 def contact_page(request):
     return render(request, "website/contact_page.html")
+
+
+class OverOnsView(generic.ListView):
+    model = Employee
+    template_name = "website/over-ons.html"
+    context_object_name = "employee_list"
+    queryset = Employee.objects.all().select_related("role")
