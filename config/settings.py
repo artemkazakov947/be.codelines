4881c3515ac627e1a7f170201edb77a7ce0365fa
Artem Kazakov <artemkazakov947@gmail.com>
2023-06-29 10:22:39 +0200

    Configurated static files

--- config/settings.py
+++ config/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "website",
+    "apps.website",
 ]
 
 MIDDLEWARE = [
@@ -55,7 +55,7 @@ ROOT_URLCONF = "config.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -116,7 +116,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "config/static/"
+
+STATICFILES_DIRS = [
+    BASE_DIR / "config" / "static",
+]
+
+# Assets Management
+ASSETS_ROOT = "/config/static/assets"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
