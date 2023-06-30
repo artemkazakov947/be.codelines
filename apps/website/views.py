0c912522f09433abd8f1a8a2d9ff162c591cc197
Artem Kazakov <artemkazakov947@gmail.com>
2023-06-30 10:16:55 +0200

    Added JobListView and JobDetailView

--- apps/website/views.py
+++ apps/website/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views import generic
 
-from apps.website.models import Employee
+from apps.website.models import Employee, Job
 
 
 def contact_page(request):
@@ -13,3 +13,13 @@ class OverOnsView(generic.ListView):
     template_name = "website/over-ons.html"
     context_object_name = "employee_list"
     queryset = Employee.objects.all().select_related("role")
+
+
+class JobListView(generic.ListView):
+    model = Job
+    queryset = Job.objects.all().prefetch_related("skill")
+
+
+class JobDetailView(generic.DetailView):
+    model = Job
+
