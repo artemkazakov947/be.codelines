3daabc94ba6576136f532e8e79144bd70accd96f
Artem Kazakov <artemkazakov947@gmail.com>
2023-06-30 10:14:42 +0200

    Registered Skill and Jod models

--- apps/website/admin.py
+++ apps/website/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 
-from apps.website.models import Role, Employee
+from apps.website.models import (Role,
+                                 Employee,
+                                 Skill,
+                                 Job)
 
 admin.site.register(Role)
 admin.site.register(Employee)
+admin.site.register(Skill)
+admin.site.register(Job)
