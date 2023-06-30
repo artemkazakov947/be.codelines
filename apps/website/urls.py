36b1621af5cef699adb97947001f5017b62047f9
Artem Kazakov <artemkazakov947@gmail.com>
2023-06-30 10:17:35 +0200

    Added url path for Jods page and Job Detail page

--- apps/website/urls.py
+++ apps/website/urls.py
@@ -3,10 +3,14 @@ from django.urls import path
 from apps.website.views import (
     contact_page,
     OverOnsView,
+    JobListView,
+    JobDetailView,
 )
 
 urlpatterns = [
     path("contact/", contact_page, name="contact_page"),
     path("over-ons/", OverOnsView.as_view(), name="over-ons"),
+    path("jobs/", JobListView.as_view(), name="job-list"),
+    path("jobs/<int:pk>/", JobDetailView.as_view(), name="job-detail"),
 ]
 app_name = "website"
