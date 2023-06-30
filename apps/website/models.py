a6e3e9f3eaa6be013a495c17c2defda5b978eb50
Artem Kazakov <artemkazakov947@gmail.com>
2023-06-30 10:24:27 +0200

    tag was added to Post model

--- apps/website/models.py
+++ apps/website/models.py
@@ -2,6 +2,7 @@ import os
 
 from django.db import models
 from django.urls import reverse_lazy
+from taggit.managers import TaggableManager
 
 
 def team_foto_file_path(instance, filename: str):
@@ -55,6 +56,7 @@ class Post(models.Model):
     content = models.TextField()
     created = models.DateField(auto_now_add=True)
     time_to_read = models.IntegerField()
+    tag = TaggableManager()
 
     def __str__(self):
         return self.title
