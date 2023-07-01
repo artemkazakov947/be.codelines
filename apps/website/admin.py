dc97edb7947ca3852d0e9f81c13a06b303ceb463
Artem Kazakov <artemkazakov947@gmail.com>
2023-07-01 16:21:24 +0200

    Registered Post model in admin panel

--- apps/website/admin.py
+++ apps/website/admin.py
@@ -3,9 +3,11 @@ from django.contrib import admin
 from apps.website.models import (Role,
                                  Employee,
                                  Skill,
-                                 Job)
+                                 Job,
+                                 Post)
 
 admin.site.register(Role)
 admin.site.register(Employee)
 admin.site.register(Skill)
 admin.site.register(Job)
+admin.site.register(Post)
