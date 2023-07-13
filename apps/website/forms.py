d962dc357d2b42e1b018792e960fff8d7ce4fdbd
Artem Kazakov <artemkazakov947@gmail.com>
2023-07-13 17:51:04 +0200

    Created RequestFromUserForm and template for this form in includes

--- apps/website/forms.py
+++ apps/website/forms.py
@@ -1,16 +1,35 @@
 from django import forms
+from phonenumber_field.formfields import PhoneNumberField
 
-from apps.website.models import EmailForPostNotification
+from apps.website.models import EmailForPostNotification, RequestFromUser
 
 
 class EmailPostNotificationForm(forms.ModelForm):
     email = forms.EmailField(
-        widget=forms.EmailInput(attrs={"class": "form-row__field form-row__mail",
-                                       "placeholder": "e-mailadres"})
+        widget=forms.EmailInput(attrs={"placeholder": "e-mailadres"})
     )
 
     class Meta:
         model = EmailForPostNotification
         fields = ("email",)
+
+
+class RequestFromUserForm(forms.ModelForm):
+    full_name = forms.CharField(
+        widget=forms.TextInput(attrs={"placeholder": "voor- en achternaam"})
+    )
+    email = forms.EmailField(
+        widget=forms.EmailInput(attrs={"placeholder": "e-mailadres"})
+    )
+    phone_number = PhoneNumberField()
+    question = forms.CharField(
+        widget=forms.TextInput(attrs={"placeholder": "mijn vraag"})
+    )
+
+    class Meta:
+        model = RequestFromUser
+        fields = "__all__"
+        exclude = ("created", )
+
 
 
