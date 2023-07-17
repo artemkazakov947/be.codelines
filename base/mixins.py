a96b12372bb4d6f52f57f5f7dbd5c959f8f17c70
Artem Kazakov <artemkazakov947@gmail.com>
2023-07-17 16:34:01 +0200

    Added BaseReadOnlyAdminMixin to make RequestFromUser model read only

--- base/mixins.py
+++ base/mixins.py
@@ -9,3 +9,14 @@ class RequestFromUserMixin(ContextMixin):
         context = super().get_context_data(object_list=None, **kwargs)
         context["form"] = RequestFromUserForm()
         return context
+
+
+class BaseReadOnlyAdminMixin:
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
