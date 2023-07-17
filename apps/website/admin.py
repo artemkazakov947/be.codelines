3c6410345f00bb26b2b1504e4d652e284ff92a81
Artem Kazakov <artemkazakov947@gmail.com>
2023-07-17 16:34:53 +0200

    Registered RequestFromUser model in admin panel

--- apps/website/admin.py
+++ apps/website/admin.py
@@ -1,27 +1,26 @@
 from django.contrib import admin
-
-from apps.website.helpers import send_new_post_notification
 from apps.website.models import (Role,
                                  Employee,
                                  Skill,
                                  Job,
                                  Post,
                                  Service,
-                                 EmailForPostNotification)
+                                 EmailForPostNotification,
+                                 Case,
+                                 RequestFromUser
+                                 )
+from base.mixins import BaseReadOnlyAdminMixin
 
 admin.site.register(Role)
 admin.site.register(Employee)
 admin.site.register(Skill)
 admin.site.register(Job)
-
-
-@admin.register(Post)
-class PostAdmin(admin.ModelAdmin):
-
-    def save_model(self, request, obj, form, change):
-        send_new_post_notification(obj)
-        super().save_model(request, obj, form, change)
-
-
+admin.site.register(Post)
 admin.site.register(Service)
 admin.site.register(EmailForPostNotification)
+admin.site.register(Case)
+
+
+@admin.register(RequestFromUser)
+class RequestFromUser(BaseReadOnlyAdminMixin, admin.ModelAdmin):
+    list_display = ("__str__",)
